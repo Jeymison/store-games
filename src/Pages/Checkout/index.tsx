72ebc5e460e3077f06c1e4d61ad5451f04903320
Jeymison Menezes <jeymisong@gmail.com>
2024-05-15 15:44:18 -0300

    feat(api): implementando a api na page checkout

--- src/Pages/Checkout/index.tsx
+++ src/Pages/Checkout/index.tsx
@@ -7,10 +7,11 @@ import boleto from '../../assets/images/boleto.png'
 import cartao from '../../assets/images/cartao.png'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
-import { scheduler } from 'timers/promises'
+import { usePurchaseMutation } from '../../services/api'
 
 const Checkout = () => {
   const [payWithCard, setPayWithCard] = useState(false)
+  const [purchase, { isLoading, isError, data }] = usePurchaseMutation()
 
   const form = useFormik({
     initialValues: {
@@ -75,7 +76,39 @@ const Checkout = () => {
     }),
 
     onSubmit: (values) => {
-      console.log(values)
+      purchase({
+        billing: {
+          name: values.fullName,
+          email: values.email,
+          document: values.cpf
+        },
+        delivery: {
+          email: values.deliveryEmail
+        },
+        payment: {
+          installments: 1,
+          card: {
+            active: payWithCard,
+            name: values.cardDisplayName,
+            number: values.cardNumber,
+            code: Number(values.cardCode),
+            owner: {
+              name: values.cardOwner,
+              document: values.cpfCardOwner
+            },
+            expires: {
+              month: 1,
+              year: 2024
+            }
+          }
+        },
+        products: [
+          {
+            id: 1,
+            price: 10
+          }
+        ]
+      })
     }
   })
 
